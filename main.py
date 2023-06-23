10595cdfa0374a0191375ae276a43139241c51ea
Vibhinn Singhal <vibhinnsinghal_ec21a18_61@dtu.ac.in>
2023-06-23 12:34:44 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import pygame as pg
+import moderngl as mgl
+import sys
+from model import *
+
+
+class GraphicsEngine:
+    def __init__(self, win_size=(1600, 900)):
+        pg.init()
+        self.WIN_SIZE = win_size
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MAJOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_MINOR_VERSION, 3)
+        pg.display.gl_set_attribute(pg.GL_CONTEXT_PROFILE_MASK, pg.GL_CONTEXT_PROFILE_CORE)
+        pg.display.set_mode(self.WIN_SIZE, flags=pg.OPENGL | pg.DOUBLEBUF)
+        self.ctx = mgl.create_context()
+        self.clock = pg.time.Clock()
+        self.scene = Triangle(self)
+
+    def check_events(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                self.scene.destroy()
+                pg.quit()
+                sys.exit()
+
+    def render(self):
+        self.ctx.clear(color=(0.08, 0.16, 0.18))
+        self.scene.render()
+        pg.display.flip()
+
+    def run(self):
+        while True:
+            self.check_events()
+            self.render()
+            self.clock.tick(60)
+
+
+if __name__ == "__main__":
+    app = GraphicsEngine()
+    app.run()
